beffdd54e797390646b2e023d14be0f5ace22da9
Animesh Shrivastava <ani979@gmail.com>
2019-03-01 21:30:11 +0530

    Stock change

---
+++ src/stock/InitialState.js
@@ -0,0 +1,30 @@
+export const initialState = {
+    cart: [],
+    stock: [
+        {
+            id: "IMM",
+            count: 12,
+            price:300
+        },
+        {
+            id: "MOISTcoconut",
+            count: 12,
+            price:300,
+            name:"Immortal",
+            flavour:"coconut"
+        },
+        {
+            id: "MOISTmango",
+            count: 12,
+            price:300,
+            name:"Immortal",
+            flavour:"mango"
+        },
+       
+        {
+            id: "LPSCRB",
+            count: 12,
+            price:100
+        },
+    ]
+}        
